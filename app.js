7b65b4ad17c7531a336a1b2380d43bbd902fe3c7
johnjunior12 <jmhaas00@gmail.com>
2017-07-24 20:18:57 -0700

    API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    // http://api.openweathermap.org/data/2.5/forecast?id=5713376&APPID=0aa02fd6e1747160cab3b61cf5073a02
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     $("#inates").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
