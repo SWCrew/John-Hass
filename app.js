c6f8d7f4ad3628863a457c7c30f1bdade48255e2
johnjunior12 <jmhaas00@gmail.com>
2017-07-24 19:29:49 -0700

    Hi Bruner

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-/* Hi John */
+$( document ).ready(function() {
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#inates").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
+});
+
